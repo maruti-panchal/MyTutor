c13a8da009f871bbd517e8bfb62c3716afaf7fc8
Maruti Panchal <112648935+maruti-panchal@users.noreply.github.com>
2024-08-22 08:57:32 +0000

    .

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -158,11 +158,10 @@ const Footer = () => {
               return (
                 <div
                   key={i}
-                  className={` ${
-                    BottomFooter.length - 1 === i
+                  className={` ${BottomFooter.length - 1 === i
                       ? ""
                       : "border-r border-richblack-700 cursor-pointer hover:text-richblack-50 transition-all duration-200"
-                  } px-3 `}
+                    } px-3 `}
                 >
                   <Link to={ele.split(" ").join("-").toLocaleLowerCase()}>
                     {ele}
